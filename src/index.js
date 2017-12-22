e1c945fef90a8b4d6bb4110e2898b0eafb1ab9cd
Michael Brooks <michael.h.b.brooks@gmail.com>
2017-12-22 16:20:33 -0500

    Initial commit. Reports version of Zed Attack Proxy.

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const { exec } = require("child_process");
+exec(
+\    "java -Xmx512m -jar zap-2.7.0.jar -version",
+    {
+        cwd: process.env.PROGRAMFILES + "\\OWASP\\Zed Attack Proxy\\"
+    },
+    (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+    }
+);
